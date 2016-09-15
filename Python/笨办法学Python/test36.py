80116d158bcfc50bd9e6003e2ee5529df53f1002
泛小七 <2750667@qq.com>
2016-09-15 19:16:19 +0800

    笨办法学Python,35节

--- Python/笨办法学Python/test36.py
+++
@@ -1,5 +0,0 @@
-#<Learn Python The Hard Way> part36
-#Customize a little game
-#Rule:
-#At the beginning of the game a few letters are given,the player uses the given letters to arrange and draw a word.
-
