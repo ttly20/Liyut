1614ccc552ae9054441a3c8482eba8d3fbea1a74
泛小七 <2750667@qq.com>
2016-09-15 14:03:22 +0800

    <天天影视>项目结构建立与文档总纲

--- Python/笨办法学Python/test35.py
+++ Python/笨办法学Python/test35.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from sys import exit
 def gold_room():
     print("This room is full of gold. How much do you take?")
---
+++ Python/笨办法学Python/test36.py
@@ -0,0 +1,5 @@
+#<Learn Python The Hard Way> part36
+#Customize a little game
+#Rule:
+#At the beginning of the game a few letters are given,the player uses the given letters to arrange and draw a word.
+
