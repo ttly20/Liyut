cb4cba4ce7b1297cc48332e79940e0208fc52ed6
泛小七 <2750667@qq.com>
2016-09-18 20:59:24 +0800

    添加Emacs缩进线功能

--- MyEmacs/lisp/init-packages.el
+++ MyEmacs/lisp/init-packages.el
@@ -5,19 +5,23 @@
 (when (>= emacs-major-version 24)
     (require 'package)
     (package-initialize)
-    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t))
+    (add-to-list 'package-archives '("melpa" . "http://elpa.zilongshanren.com/melpa/") t))
 ;;cl-Common Lisp扩展
 (require 'cl)
 ;;添加插件源与插件列表
 (defvar Liyut_packages '(
 			 ;;自动补全
 			 company
+			 yasnippet
+             ;;js-mode
 			 js2-mode
 			 nodejs-repl
-			 yasnippet
+             ;;代码跟踪
 			 cedet
 			 ;;中英文对齐
 			 chinese-fonts-setup
+             ;;缩进线
+             highlight-indentation
 			 ) "Default packages")
 ;;自动安装缺少的插件
 (defun Liyut_packages_installed_p ()
@@ -43,6 +47,16 @@
 (yas-reload-all t)
 (add-hook 'prog-mode-hook #'yas-minor-mode)
 
+;;加载表格对齐配置
 (require 'chinese-fonts-setup)
+
+;;缩进线加载
+(require 'highlight-indentation)
+(set-face-background 'highlight-indentation-face "#1E90FF")
+;;(set-face-background 'highlight-indentation-current-column-face "blue")
+(add-hook 'c-mode-common-hook 'highlight-indentation-mode)
+(add-hook 'emacs-lisp-mode-hook 'highlight-indentation-mode)
+(add-hook 'python-mode-hook 'highlight-indentation-mode)
+(add-hook 'html-mode-hook 'highlight-indentation-mode)
 
 (provide 'init-packages)
